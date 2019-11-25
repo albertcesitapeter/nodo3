54c0a7f80f01712ebe0e888d93665397d6c9cb18
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2019-11-25 12:21:51 -0400

    prueba port

--- config/config.js
+++ config/config.js
@@ -1,16 +1,16 @@
 
 //PAPACALIENTE profiles
-process.env.PORT = 10003;
-process.env.NAMENODE = "JUGADOR 1";
+/* process.env.PORT = 10003;
+process.env.NAMENODE = "JUGADOR 1"; */
 
 // next player to send papa
-process.env.NEXT = 10002;
+/* process.env.NEXT = 10002; */
 
 
 //arreglo de puertos
 //esto despues se le coloca ip
 // TODO: REVISARRRRRRRRRRRRRRRRRRRRRRRR
-process.env.PORTLIST = [
+/* process.env.PORTLIST = [
     "192.168.1.101:10001",
     "10002"
-]
+] */
